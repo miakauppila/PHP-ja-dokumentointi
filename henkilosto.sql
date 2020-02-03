587ac9f2c8ce2917dbaa85b96a3c6fd19eb421c8
Mia Kauppila <mia.kauppila@gmail.com>
2020-02-03 11:34:18 +0200

    Update henkilosto.sql

--- henkilosto.sql
+++ henkilosto.sql
@@ -1,3 +1,4 @@
+-- Copy this to create the database:
 CREATE TABLE henkilosto(
 henkiloNumero INTEGER NOT NULL PRIMARY KEY,
 etunimi VARCHAR(20) NOT NULL,
@@ -7,6 +8,7 @@ osasto VARCHAR(15),
 palkka DECIMAL(6,2)
 );
 
+-- Copy this only if you want to insert data in phpMyAdmin:
 INSERT INTO henkilosto VALUES
 ('1', 'Matti', 'Puro', '0403234255', 'IT', '4400.00'), 
 ('2', 'Pirjo', 'Hakkila', '0445144086', 'Hallinto', '4100.00'),
